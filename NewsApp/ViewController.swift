6d43628bfe7c9c833b2c4f684c97646db25fbbf0
Vitor Henrique <vhenriquebm@gmail.com>
2022-09-21 20:59:47 -0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Vitor Henrique Barreiro Marinho on 21/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
